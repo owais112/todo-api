93965cfba16afdfbe639554f547fcc90ec168341
Owais Malik <Owais Malik>
2017-04-08 11:55:20 +0500

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -19,10 +19,20 @@ app.post('/todos' , (req , res) => {
     });
 });
 
+app.get('/todos' , (req , res) => {
+    Todo.find().then((todo) => {
+        res.send({todo});
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
 });
+
+module.exports = {app};
  
 
 
